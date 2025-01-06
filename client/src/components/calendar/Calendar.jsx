490688d63dd8221859f70958b2396b20104f84de
Joseph Hofmeister <jphofmeister@gmail.com>
2025-01-05 22:54:58 -0500

    start work on updating to new calendar

--- client/src/components/calendar/Calendar.jsx
+++ client/src/components/calendar/Calendar.jsx
@@ -1,60 +1,54 @@
-import React, { useState, useEffect, useRef } from "react";
-// import classnames from "classnames";
-import { noFunctionAvailable, isEmpty, displayDate, formatToString, isNonEmptyArray } from "../../utilities/sharedFunctions";
-import { calculateDate, displayMonthName, getStartOfMonth, getEndOfMonth, getEachDayOfInterval, displayDay, checkIsSameDay } from "./DateFunctions";
+import React, { useState, useEffect } from "react";
+import { isEmpty, getDateTime, isNonEmptyArray } from "../../utilities/sharedFunctions";
+import { calculateDate, getStartOfMonth, getEndOfMonth, getEachDayOfInterval, checkIsSameDay } from "./DateFunctions";
 import CalendarCell from "./CalendarCell";
+import CalendarWeekEvents from "./CalendarWeekEvents";
 
 const Calendar = (props) => {
 
   // * Available props: -- 07/01/2024 JH
   // * Properties: currentRangeStart, calendarEvents -- 07/01/2024 JH
-  // * Functions: getStatus -- 07/01/2024 JH
 
   let componentName = "Calendar";
-
-  const calendarRef = useRef(null);
 
   let currentRangeStart = isEmpty(props.currentRangeStart) === false ? props.currentRangeStart : null;
   let calendarEvents = isEmpty(props.calendarEvents) === false ? props.calendarEvents : null;
 
-  let getStatus = isEmpty(props) === false && isEmpty(props.getStatus) === false ? props.getStatus : noFunctionAvailable;
-
   const [weeksInMonth, setWeeksInMonth] = useState([]);
-  const [calendarWidth, setCalendarWidth] = useState(0);
 
   let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 
-  // * Get width of calendar container for responsive calculations -- 07/07/2024 JH
-  useEffect(() => {
+  // // * Get width of calendar container for responsive calculations -- 07/07/2024 JH
+  // useEffect(() => {
 
-    if (isEmpty(calendarRef) === false && isEmpty(calendarRef.current) === false) {
+  //   if (isEmpty(calendarRef) === false && isEmpty(calendarRef.current) === false) {
 
-      // * https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver -- 07/08/2024 JH
-      const resizeObserver = new ResizeObserver(() => {
+  //     // * https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver -- 07/08/2024 JH
+  //     const resizeObserver = new ResizeObserver(() => {
 
-        if (calendarRef.current.offsetWidth !== calendarWidth) {
+  //       if (calendarRef.current.offsetWidth !== calendarWidth) {
 
-          setCalendarWidth(calendarRef.current.offsetWidth);
+  //         setCalendarWidth(calendarRef.current.offsetWidth);
 
-        };
+  //       };
 
-      });
+  //     });
 
-      resizeObserver.observe(calendarRef.current);
+  //     resizeObserver.observe(calendarRef.current);
 
-      return () => {
+  //     return () => {
 
-        resizeObserver.disconnect();
+  //       resizeObserver.disconnect();
 
-      };
+  //     };
 
-    };
+  //   };
 
-  }, [calendarRef]);
+  // }, [calendarRef]);
 
 
-  // * Get dates to display in calendar -- 07/01/2024 JH
+  // * Get dates to display in calendar. -- 07/01/2024 JH
   useEffect(() => {
 
     if (isEmpty(currentRangeStart) === false) {
@@ -70,7 +64,7 @@ const Calendar = (props) => {
   }, [currentRangeStart]);
 
 
-  // * Get an array of all days for this month -- 07/10/2024 JH
+  // * Get an array of all days for this month. -- 07/10/2024 JH
   const getAllDaysOfMonth = (dayOfThisMonth) => {
 
     let currentDay = calculateDate(dayOfThisMonth);
@@ -81,7 +75,7 @@ const Calendar = (props) => {
     let startDayIndex = startOfMonth.getDay();
     let endDayIndex = endOfMonth.getDay();
 
-    // * If the first of the month is not on Sunday, get the last days of the previous month for this week -- 07/10/2024 JH
+    // * If the first of the month is not on Sunday, get the last days of the previous month for this week. -- 07/10/2024 JH
     if (startDayIndex > 0) {
 
       let daysToSubtract = startDayIndex * -1;
@@ -92,7 +86,7 @@ const Calendar = (props) => {
 
     };
 
-    // * If the last of the month is not on Saturday, get the first days of the next month for the rest of this week -- 07/10/2024 JH
+    // * If the last of the month is not on Saturday, get the first days of the next month for the rest of this week. -- 07/10/2024 JH
     if (endDayIndex < 6) {
 
       let daysToAdd = 6 - endDayIndex;
@@ -108,7 +102,7 @@ const Calendar = (props) => {
   };
 
 
-  // * Divide the days of the month into weeks, returning an array of arrays -- 07/10/2024 JH
+  // * Divide the days of the month into weeks, returning an array of arrays. -- 07/10/2024 JH
   const divideDaysIntoWeeks = (days) => {
 
     let newWeeksInMonth = [];
@@ -116,17 +110,17 @@ const Calendar = (props) => {
 
     for (let i = 0; i < days.length; i++) {
 
-      // * If first day of week, create empty array for the new week -- 07/10/2024 JH
+      // * If first day of week, create empty array for the new week. -- 07/10/2024 JH
       if (days[i].getDay() === 0) {
 
         newWeeksInMonth[weekIndex] = [];
 
       };
 
-      // * Push day to this weekIndex's array -- 07/10/2024 JH
+      // * Push day to this weekIndex's array. -- 07/10/2024 JH
       newWeeksInMonth[weekIndex].push(days[i]);
 
-      // * If last day of week, increment weekIndex to start adding days to the next week -- 07/10/2024 JH
+      // * If last day of week, increment weekIndex to start adding days to the next week. -- 07/10/2024 JH
       if (days[i].getDay() === 6) {
 
         weekIndex++;
@@ -140,8 +134,8 @@ const Calendar = (props) => {
   };
 
 
-  // * Used to filter calendarEvents that occur within a week, including any that run over from the previous week -- 07/25/2024 JH
-  // ? Maybe move into DateFunctions.js? But not sure if name is accurate to what would be expected -- 07/25/2024 JH
+  // * Used to filter calendarEvents that occur within a week, including any that run over from the previous week. -- 07/25/2024 JH
+  // ? Maybe move into DateFunctions.js? But not sure if name is accurate to what would be expected. -- 07/25/2024 JH
   const checkIsInDateRange = (dateToCompare, dateRangeStart, dateRangeEnd) => {
 
     let thisDayIndex = dateToCompare.getDay();
@@ -154,7 +148,7 @@ const Calendar = (props) => {
 
     } else {
 
-      // * Check if date started in previous week and continued into this week -- 07/25/2024 JH
+      // * Check if date started in previous week and continued into this week. -- 07/25/2024 JH
       if (thisDayIndex === 0) {
 
         let dateRange = getEachDayOfInterval(dateRangeStart, dateRangeEnd);
@@ -183,33 +177,33 @@ const Calendar = (props) => {
 
 
   return (
-    <table className="calendar" ref={calendarRef}>
+    <div className="calendar">
 
-      <thead>
-        <tr>
+      <div className="calendar__thead">
+        <div className="calendar__tr">
+
           {weekdays.map((day) => {
             return (
-              <th className="calendar__cell calendar__cell--heading" key={day}>{day}</th>
+              <div className="calendar__th" key={day}>{day}</div>
             );
           })}
-        </tr>
-      </thead>
+
+        </div>
+      </div>
 
       {isNonEmptyArray(weeksInMonth) === true && isNonEmptyArray(calendarEvents) === true ?
 
-        <tbody>
+        <div className="calendar__tbody">
 
-          {/* // * This is mapping through an array of arrays -- 07/10/2024 JH */}
+          {/* // * This is mapping through an array of arrays. -- 07/10/2024 JH */}
           {weeksInMonth.map((week, index) => {
 
-            console.log("weeksInMonth", weeksInMonth);
-
-            // * Need a flat array of the unique events in this week -- 07/25/2024 JH
+            // * Need a flat array of the unique events in this week. -- 07/25/2024 JH
             let thisWeeksEvents = week.flatMap(day => {
 
               let thisDaysEvents = calendarEvents.filter((event) => checkIsInDateRange(day, event.calendarStartDate, event.calendarEndDate) === true);
 
-              // * In case there are events from the previous week, sort to place them at the top -- 07/11/2024 JH
+              // * In case there are events from the previous week, sort to place them at the top. -- 07/11/2024 JH
               if (isNonEmptyArray(thisDaysEvents) === true) {
 
                 // * sortObjectArrayByProperty can't sort dates? -- 07/11/2024 JH
@@ -224,35 +218,30 @@ const Calendar = (props) => {
             });
 
             return (
-              <tr key={index}>
+              <div key={index} className="calendar__tr-container">
 
-                {week.map(day => {
+                <div className="calendar__tr">
 
-                  let thisDaysEvents = thisWeeksEvents.filter((event) => checkIsInDateRange(day, event.calendarStartDate, event.calendarEndDate) === true);
+                  {week.map(day => {
+                    return (
+                      <CalendarCell key={day} day={day} currentRangeStart={currentRangeStart} />
+                    );
+                  })}
 
-                  return (
-                    <CalendarCell
-                      key={day}
-                      day={day}
-                      thisDaysEvents={thisDaysEvents}
-                      thisWeeksEvents={thisWeeksEvents}
-                      calendarWidth={calendarWidth}
-                      currentRangeStart={currentRangeStart}
-                      getStatus={getStatus} />
-                  );
+                </div>
 
-                })}
+                <CalendarWeekEvents key={index} week={week} thisWeeksEvents={thisWeeksEvents} />
 
-              </tr>
+              </div>
             );
 
           })}
 
-        </tbody>
+        </div>
 
         : null}
 
-    </table>
+    </div>
   );
 };
 
---
+++ client/src/components/calendar/CalendarWeekEvents.jsx
@@ -0,0 +1,138 @@
+import React, { useEffect } from "react";
+import classnames from "classnames";
+import { isEmpty, getDateTime, isNonEmptyArray } from "../../utilities/sharedFunctions";
+import { calculateDate, getEachDayOfInterval, displayDay, checkIsSameDay } from "./DateFunctions";
+import CalendarItem from "./CalendarItem";
+
+const CalendarWeekEvents = (props) => {
+
+  // * Available props: -- 07/11/2024 JH
+  // * Properties: week, thisWeeksEvents -- 07/11/2024 JH
+
+  let componentName = "CalendarWeekEvents";
+
+  let week = isEmpty(props.week) === false ? props.week : [];
+  let thisWeeksEvents = isEmpty(props.thisWeeksEvents) === false ? props.thisWeeksEvents : [];
+
+
+  return (
+    <React.Fragment>
+
+      {isNonEmptyArray(thisWeeksEvents) === true ?
+
+        <div className="calendar__week-events">
+
+          {thisWeeksEvents.map((eventItem) => {
+
+            // * The code below is all used for calculating the styles of the eventItem. -- 07/25/2024 JH
+            let eventDisplayType = "";
+
+            let gridColumnStart = 0;
+            let gridColumnEnd = 0;
+
+            let startIndex = new Date(eventItem.calendarStartDate).getDay();
+            let endIndex = new Date(eventItem.calendarEndDate).getDay();
+
+            let startDayIsInThisWeek = false;
+            let endDayIsInThisWeek = false;
+
+            week.forEach(day => {
+
+              if (checkIsSameDay(day, eventItem.calendarStartDate) === true) {
+
+                startDayIsInThisWeek = true;
+
+              };
+
+              if (checkIsSameDay(day, eventItem.calendarEndDate) === true) {
+
+                endDayIsInThisWeek = true;
+
+              };
+
+            });
+
+            if (startDayIsInThisWeek === true) {
+
+              if (endDayIsInThisWeek !== true) {
+
+                // * Event starts this week, but doesn't end this week. -- 07/11/2024 JH
+
+                gridColumnStart = startIndex + 1;
+
+                gridColumnEnd = 8;
+
+                eventDisplayType = "partial-start";
+
+              } else {
+
+                // * Event starts and ends within same week. -- 07/11/2024 JH
+
+                gridColumnStart = startIndex + 1;
+
+                gridColumnEnd = endIndex + 2;
+
+              };
+
+            } else {
+
+              if (endDayIsInThisWeek !== true) {
+
+                // * Event doesn't start or end this week. -- 07/11/2024 JH
+
+                eventDisplayType = "partial-start-end";
+
+                gridColumnStart = 1;
+
+                gridColumnEnd = 8;
+
+              } else {
+
+                // * Event doesn't start this week, but ends this week. -- 07/11/2024 JH
+
+                eventDisplayType = "partial-end";
+
+                gridColumnStart = 1;
+
+                gridColumnEnd = endIndex + 2;
+
+              };
+
+            };
+
+            let eventOfThisWeekIndex = thisWeeksEvents.findIndex((weekEvent) => weekEvent.calendarID === eventItem.calendarID);
+
+            let eventColor = isEmpty(eventItem.request) === false && isEmpty(eventItem.request.eventColor) === false ? eventItem.request.eventColor : "#1c1b1e";
+
+            let eventItemContainerStyles = {
+              borderColor: `${eventColor}`,
+              gridColumn: `${gridColumnStart} / ${gridColumnEnd}`,
+              gridRow: eventOfThisWeekIndex + 1
+            };
+
+            let eventItemStyles = {
+              borderColor: `${eventColor}`
+            };
+
+            let eventClasses = classnames("event-item", {
+              "event-item--partial-start": eventDisplayType === "partial-start",
+              "event-item--partial-end": eventDisplayType === "partial-end",
+              "event-item--partial-start event-item--partial-end": eventDisplayType === "partial-start-end"
+            });
+
+            return (
+              <CalendarItem key={eventItem.calendarID} eventItem={eventItem} eventClasses={eventClasses} eventItemStyles={eventItemStyles} eventItemContainerStyles={eventItemContainerStyles} />
+            );
+
+          })}
+
+        </div>
+
+        : null}
+
+    </React.Fragment>
+  );
+
+};
+
+export default CalendarWeekEvents;
